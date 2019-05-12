7a62d8c8d31e7f572a31e6ecd25cde7a1beb7c5f
bbondd <jo_chestnut@naver.com>
2019-05-12 23:45:31 +0900

    removed useless files

--- TFNST.py
+++ TFNST.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import tensorflow.contrib.eager as tfe
 from tensorflow.python import keras as kr
 from tensorflow.python.keras.preprocessing import image
 import numpy as np
@@ -7,34 +6,34 @@ import numpy as np
 tf.enable_eager_execution()
 
 content_path = './pictures/content.jpg'
-style_path = './pictures/style.jpg'
+style_paths = ['./pictures/style1.jpg']
+
+
+def load_and_process_image(path):
+    image_array = image.img_to_array(image.load_img(path))
+    return kr.applications.vgg19.preprocess_input(image_array)
+
+
+content_array = load_and_process_image(content_path)
+style_arrays = [load_and_process_image(style_path) for style_path in style_paths]
+
 
 content_layers = ['block5_conv2']
-
 style_layers = ['block1_conv1',
                 'block2_conv1',
                 'block3_conv1',
                 'block4_conv1',
                 'block5_conv1',]
 
+optimizer = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=1e-1)
 
-def load_and_process_image(path):
-    image_array = image.img_to_array(image.load_img(path))
-    image_array = kr.applications.vgg19.preprocess_input(image_array)
-    return np.expand_dims(image_array, axis=0)
-
-
-content_array = load_and_process_image(content_path)
-style_array = load_and_process_image(style_path)
-
-
-style_weight = 1.0
-content_weight = 0.01
-iteration_size = 500
+style_weight = 1.0 / len(style_arrays)
+content_weight = 0.03
+iteration_size = 1501
 
 
 def deprocess_image(image_array):
-    x = image_array[0]
+    x = image_array
     x[:, :, 0] += 103.939
     x[:, :, 1] += 116.779
     x[:, :, 2] += 123.68
@@ -46,17 +45,17 @@ def deprocess_image(image_array):
 
 def get_model():
     vgg = kr.applications.VGG19(include_top=False, weights='imagenet')
-    vgg.trainable = False
 
     style_outputs = [vgg.get_layer(name).output for name in style_layers]
     content_outputs = [vgg.get_layer(name).output for name in content_layers]
     model_outputs = style_outputs + content_outputs
 
-    return kr.Model(vgg.input, model_outputs)
+    model = kr.Model(vgg.input, model_outputs)
 
+    for layer in model.layers:
+        layer.trainable = False
 
-def get_content_loss(base_content, target):
-    return tf.reduce_mean(tf.square(base_content - target))
+    return model
 
 
 def gram_matrix(input_tensor):
@@ -66,79 +65,54 @@ def gram_matrix(input_tensor):
     return gram / tf.cast(n, tf.float32)
 
 
-def get_style_loss(base_style, gram_target):
-    return tf.reduce_mean(tf.square(gram_matrix(base_style) - gram_target))
-
-
 def get_feature_representations(model):
-    style_outputs = model(style_array)
-    content_outputs = model(content_array)
+    style_features_group = []
+    for style_array in style_arrays:
+        style_outputs = model(np.expand_dims(style_array, axis=0))
+        style_features_group.append([style_layer[0] for style_layer in style_outputs[:len(style_layers)]])
 
-    style_features = [style_layer[0] for style_layer in style_outputs[:len(style_layers)]]
+    content_outputs = model(np.expand_dims(content_array, axis=0))
+
     content_features = [content_layer[0] for content_layer in content_outputs[len(style_layers):]]
 
-    return style_features, content_features
+    return style_features_group, content_features
 
 
-def compute_loss(model, init_image, gram_style_features, content_features):
-    model_outputs = model(init_image)
-
-    style_output_features = model_outputs[:len(style_layers)]
-    content_output_features = model_outputs[len(style_layers):]
-
-    style_score, content_score = 0, 0
-
-    for target_style, comb_style in zip(gram_style_features, style_output_features):
-        style_score += get_style_loss(comb_style[0], target_style)
-
-    for target_content, comb_content in zip(content_features, content_output_features):
-        content_score += get_content_loss(comb_content[0], target_content)
-
-    style_score *= style_weight
-    content_score *= content_weight
-
-    loss = style_score + content_score
-
-    return loss, style_score, content_score
-
-
-def compute_grads(model, init_image, gram_style_features, content_features):
+def get_gradient(model, combined_array, style_features_group, content_features):
     with tf.GradientTape() as tape:
-        all_loss = compute_loss(model, init_image, gram_style_features, content_features)
+        model_outputs = model(combined_array)
 
-    total_loss = all_loss[0]
+        combined_style_features = model_outputs[:len(style_layers)]
+        combined_content_features = model_outputs[len(style_layers):]
 
-    return tape.gradient(total_loss, init_image), all_loss
+        style_loss, content_loss = 0, 0
+
+        for style_features in style_features_group:
+            for style_feature, comb_style in zip(style_features, combined_style_features):
+                style_loss += tf.reduce_mean(tf.square(gram_matrix(style_feature) - gram_matrix(comb_style)))
+
+        for target_content, comb_content in zip(content_features, combined_content_features):
+            content_loss += tf.reduce_mean(tf.square(comb_content[0], target_content))
+
+        loss = style_loss * style_weight + content_loss * content_weight
+
+    return tape.gradient(loss, combined_array)
 
 
-def run_style_transfer():
+def main():
     model = get_model()
-    for layer in model.layers:
-        layer.trainable = False
 
-    style_features, content_features = get_feature_representations(model)
-    gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
+    style_features_group, content_features = get_feature_representations(model)
 
-    init_image = tfe.Variable(content_array, dtype=tf.float32)
+    combined_array = tf.contrib.eager.Variable(np.expand_dims(content_array, axis=0), dtype=tf.float32)
 
-    opt = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=1e-1)
+    for i in range(iteration_size):
+        gradients = get_gradient(model, combined_array, style_features_group, content_features)
+        optimizer.apply_gradients([(gradients, combined_array)])
 
-    loss_weights = (style_weight, content_weight)
-
-    norm_means = np.array([103.939, 116.779, 123.68])
-    min_vals = -norm_means
-    max_vals = 255 - norm_means
-
-    for _ in range(iteration_size):
-        grades, all_loss = compute_grads(model, init_image, gram_style_features, content_features)
-        loss, style_score, content_score = all_loss
-        opt.apply_gradients([(grades, init_image)])
-        clipped = tf.clip_by_value(init_image, min_vals, max_vals)
-        init_image.assign(clipped)
-
-    return deprocess_image(init_image.numpy())
+        if i % 50 == 0:
+            image.save_img(x=image.array_to_img(deprocess_image(combined_array.numpy()[0])),
+                           path='./combined/combine%d.jpg' % i)
 
 
-combined_array = run_style_transfer()
-image.save_img(x=image.array_to_img(combined_array), path='./combined/combine.jpg')
-
+main()
